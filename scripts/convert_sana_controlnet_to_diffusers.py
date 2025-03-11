af89968fbef4d38ccbdd2dad8ffdf19cd6b20b19
ishan-modi <ishan.modi24@gmail.com>
2025-03-11 20:21:48 +0530

    test

--- scripts/convert_sana_controlnet_to_diffusers.py
+++ scripts/convert_sana_controlnet_to_diffusers.py
@@ -174,7 +174,7 @@ def main(args):
     print(controlnet)
     controlnet.load_state_dict(converted_state_dict, strict=True)
 
-    print(f"Saving Sana ControlNet in Diffusers format in {args.output_path}.")
+    print(f"Saving Sana ControlNet in Diffusers format in {args.dump_path}.")
     controlnet.save_pretrained(args.dump_path)
 
 
