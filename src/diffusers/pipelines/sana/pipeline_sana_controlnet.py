f40ee751f2b474028e11aded587a9ac8077cbd4f
ishan-modi <ishan.modi24@gmail.com>
2025-03-12 20:46:33 +0530

    test

--- src/diffusers/pipelines/sana/pipeline_sana_controlnet.py
+++ src/diffusers/pipelines/sana/pipeline_sana_controlnet.py
@@ -1016,8 +1016,6 @@ class SanaControlNetPipeline(DiffusionPipeline, SanaLoraLoaderMixin):
                 # learned sigma
                 if self.transformer.config.out_channels // 2 == latent_channels:
                     noise_pred = noise_pred.chunk(2, dim=1)[0]
-                else:
-                    noise_pred = noise_pred
 
                 # compute previous image: x_t -> x_t-1
                 latents = self.scheduler.step(noise_pred, t, latents, **extra_step_kwargs, return_dict=False)[0]
