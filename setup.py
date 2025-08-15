055e9d986c04a61a421df7abf0ce285bcf4d5d9d
ishan-modi <ishan.modi24@gmail.com>
2025-08-15 15:01:55 +0530

    update

--- setup.py
+++ setup.py
@@ -245,7 +245,7 @@ extras["bitsandbytes"] = deps_list("bitsandbytes", "accelerate")
 extras["gguf"] = deps_list("gguf", "accelerate")
 extras["optimum_quanto"] = deps_list("optimum_quanto", "accelerate")
 extras["torchao"] = deps_list("torchao", "accelerate")
-extras["nvidia_modelopt"] = deps_list("nvidia_modelopt[torch, hf]")
+extras["nvidia_modelopt"] = deps_list("nvidia_modelopt[hf]")
 
 if os.name == "nt":  # windows
     extras["flax"] = []  # jax is not supported on windows
